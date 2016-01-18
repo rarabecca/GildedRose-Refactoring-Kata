c8813e1087a34b221116f13b5ca6cd059e24c5e9
Rebecca Olson <rarabecca@gmail.com>
2016-01-17 22:43:34 -0600

    Moved Item to a shared package as to not upset the goblin. Changed the array of items to list of items to make use of collections

--- Java/com/gildedrose/GildedRoseTest.java
+++ Java/com/gildedrose/GildedRoseTest.java
@@ -2,16 +2,51 @@ package com.gildedrose;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
+import com.gildedrose.shared.Item;
 
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void testFoo() {
+    	List<Item> items = new ArrayList<Item>();
+        Item item = new Item("foo", 0, 0);
+        items.add(item);
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("foo", app.items.get(0).name);
     }
+    
+    @Test
+    public void TestQualityImprovementOnceForBrie() {
+    	List<Item> items = new ArrayList<Item>();
+        Item item = new Item("Aged Brie", 0, 0);
+        items.add(item);
+    	GildedRose app = new GildedRose(items);
+    	app.updateQuality();
+    	assertEquals(app.items.get(0).quality, 2);
+    }
+    
+    @Test
+    public void TestQualityImprovementForBrieMultiple() {
+    	List<Item> items = new ArrayList<Item>();
+        Item item = new Item("Aged Brie", 0, 0);
+        items.add(item);
+        GildedRose app = new GildedRose(items);
+    	int i = 0;
+    	while (app.items.get(0).quality != 50) {		
+    		app.updateQuality();
+    		i= i+2;
+    		System.out.println(app.items.get(0).quality);
+        	assertEquals(app.items.get(0).quality, i);
+    	}
+    	assertEquals(app.items.get(0).quality, 50); 	
+    }
+    
+    
 
 }
--- Java/com/gildedrose/TexttestFixture.java
+++ Java/com/gildedrose/TexttestFixture.java
@@ -1,24 +1,30 @@
 package com.gildedrose;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.gildedrose.shared.Item;
+
 public class TexttestFixture {
     public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
-        Item[] items = new Item[] {
-                new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
-                new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+        List<Item> items = new ArrayList<Item>();
+        
+                items.add(new Item("+5 Dexterity Vest", 10, 20)); //
+                items.add(new Item("Aged Brie", 2, 0)); //
+                items.add(new Item("Elixir of the Mongoose", 5, 7));//
+                items.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80)); //
+                items.add(new Item("Sulfuras, Hand of Ragnaros", -1, 80));
+                items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+                items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49));
+                items.add(new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49));
                 // this conjured item does not work properly yet
-                new Item("Conjured Mana Cake", 3, 6) };
+                items.add(new Item("Conjured Mana Cake", 3, 6));
 
         GildedRose app = new GildedRose(items);
 
-        int days = 2;
+        int days = 4;
         if (args.length > 0) {
             days = Integer.parseInt(args[0]) + 1;
         }
